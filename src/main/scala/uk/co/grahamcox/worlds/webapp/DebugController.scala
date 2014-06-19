ad6a9a99c7ea84e27043166b1642ce227c8fc380
Graham Cox <graham@grahamcox.co.uk>
2014-06-19 20:54:09 +0100

    Added api method to get the current time

--- src/main/scala/uk/co/grahamcox/worlds/webapp/DebugController.scala
+++ src/main/scala/uk/co/grahamcox/worlds/webapp/DebugController.scala
@@ -1,5 +1,7 @@
 package uk.co.grahamcox.worlds.webapp
 
+import java.time.format.DateTimeFormatter
+import java.time.{ZonedDateTime, Clock}
 import java.util.Properties
 
 import org.springframework.stereotype.Controller
@@ -7,16 +9,30 @@ import org.springframework.web.bind.annotation.{ResponseBody, RequestMapping}
 
 /**
  * Controller to get debug information
+ * @param buildInfo The build information
+ * @param clock The clock
  */
 @Controller
 @RequestMapping(Array("/api/debug"))
-class DebugController(private val buildInfo: Properties) {
+class DebugController(private val buildInfo: Properties,
+                       private val clock: Clock) {
   /**
    * Get the debug information for the webapp
+   * @return the build information
    */
   @RequestMapping(Array("/info"))
   @ResponseBody
   def info = {
     buildInfo
+  }
+
+  /**
+   * Get the current time according to the server
+   * @return the current time
+   */
+  @RequestMapping(Array("/now"))
+  @ResponseBody
+  def now = {
+    ZonedDateTime.now(clock).format(DateTimeFormatter.ISO_DATE_TIME)
   }
 }
--- src/test/scala/uk/co/grahamcox/worlds/webapp/DebugControllerSpec.scala
+++ src/test/scala/uk/co/grahamcox/worlds/webapp/DebugControllerSpec.scala
@@ -1,5 +1,7 @@
 package uk.co.grahamcox.worlds.webapp
 
+import java.time.format.DateTimeFormatter
+
 import org.scalatest.{FunSpec, Matchers}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.{TestContextManager, ActiveProfiles, ContextConfiguration}
@@ -27,27 +29,40 @@ class DebugControllerSpec extends FunSpec with Matchers {
     val mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
 
     describe("/info") {
-      describe("When requesting JSON") {
-        val result = mockMvc.perform(MockMvcRequestBuilders.get("/api/debug/info"))
+      val result = mockMvc.perform(MockMvcRequestBuilders.get("/api/debug/info"))
 
-        it("Should return success") {
-          result.andExpect(MockMvcResultMatchers.status().isOk)
-        }
-        it("Should return JSON") {
-          result.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith("application/json"))
-        }
-        it("Should return build.time") {
-          result.andExpect(MockMvcResultMatchers.jsonPath("$.[build.time]").value("19 June 2014, 12:52:52 +01:00"))
-        }
-        it("Should return project.name") {
-          result.andExpect(MockMvcResultMatchers.jsonPath("$.[project.name]").value("Multiworld Online"))
-        }
-        it("Should return project.version") {
-          result.andExpect(MockMvcResultMatchers.jsonPath("$.[project.version]").value("1.0-SNAPSHOT"))
-        }
-        it("Should return git.revision") {
-          result.andExpect(MockMvcResultMatchers.jsonPath("$.[git.revision]").value("4e5f3e24bd95b54c6f47a9b0e5fdcc70b4169fb2"))
-        }
+      it("Should return success") {
+        result.andExpect(MockMvcResultMatchers.status().isOk)
+      }
+      it("Should return JSON") {
+        result.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith("application/json"))
+      }
+      it("Should return build.time") {
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.[build.time]").value("19 June 2014, 12:52:52 +01:00"))
+      }
+      it("Should return project.name") {
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.[project.name]").value("Multiworld Online"))
+      }
+      it("Should return project.version") {
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.[project.version]").value("1.0-SNAPSHOT"))
+      }
+      it("Should return git.revision") {
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.[git.revision]").value("4e5f3e24bd95b54c6f47a9b0e5fdcc70b4169fb2"))
+      }
+    }
+
+    describe("/now") {
+      val result = mockMvc.perform(MockMvcRequestBuilders.get("/api/debug/now"))
+
+      it("Should return success") {
+        result.andExpect(MockMvcResultMatchers.status().isOk)
+      }
+      it("Should return text") {
+        result.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith("text/plain"))
+      }
+      it("Should be a valid time") {
+        val content = result.andReturn().getResponse.getContentAsString
+        DateTimeFormatter.ISO_DATE_TIME.parse(content)
       }
     }
   }
