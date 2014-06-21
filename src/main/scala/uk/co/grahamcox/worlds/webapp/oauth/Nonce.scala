3ebac08d410988851877070a99f903ae6d37485c
Graham Cox <graham@grahamcox.co.uk>
2014-06-21 17:32:09 +0100

    Nonce storage and generation

---
+++ src/main/scala/uk/co/grahamcox/worlds/webapp/oauth/Nonce.scala
@@ -0,0 +1,29 @@
+package uk.co.grahamcox.worlds.webapp.oauth
+
+import java.util.UUID
+
+/**
+ * Wrapper around the Nonce value to use
+ */
+case class Nonce(val value: String)
+
+/**
+ * Generator to generate a Nonce to use
+ */
+trait NonceGenerator {
+  /**
+   * Actually generate a Nonce
+   * @return the nonce to use
+   */
+  def generate: Nonce
+}
+
+/**
+ * Nonce generator that will generate a Nonce from a newly generated UUID
+ */
+class UuidNonceGenerator extends NonceGenerator {
+  /**
+   * {@inheritdoc}
+   */
+  override def generate = Nonce(UUID.randomUUID().toString)
+}
---
+++ src/test/scala/uk/co/grahamcox/worlds/webapp/oauth/Nonce.scala
@@ -0,0 +1,24 @@
+package uk.co.grahamcox.worlds.webapp.oauth
+
+import org.scalatest.{ShouldMatchers, FunSpec}
+
+/**
+ * Created by graham on 21/06/14.
+ */
+class UuidNonceGeneratorSpec extends FunSpec with ShouldMatchers {
+  describe("UuidNonceGenerator") {
+    val generator = new UuidNonceGenerator
+    describe("When generating a Nonce") {
+      val nonce = generator.generate
+
+      it("Should be a valid Nonce") {
+        nonce should be(a[Nonce])
+      }
+
+      it("Should have a valid UUID as the value") {
+        val value = nonce.value
+        value should fullyMatch regex("""[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}""")
+      }
+    }
+  }
+}
