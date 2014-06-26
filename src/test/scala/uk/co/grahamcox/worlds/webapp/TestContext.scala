49f4e64db488b5d90bb69670f85777db0d35cbc6
Graham Cox <graham@grahamcox.co.uk>
2014-06-26 20:06:24 +0100

    Non-working attempt at getting external configuration working

--- src/test/scala/uk/co/grahamcox/worlds/webapp/TestContext.scala
+++ src/test/scala/uk/co/grahamcox/worlds/webapp/TestContext.scala
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.{ImportResource, Configuration}
 
 @Configuration
 @ImportResource(Array(
+  "classpath:/uk/co/grahamcox/worlds/test-context.xml",
   "classpath:/uk/co/grahamcox/worlds/webapp/core/config/context.xml",
   "classpath:/uk/co/grahamcox/worlds/webapp/webapp/config/context.xml"
 ))
